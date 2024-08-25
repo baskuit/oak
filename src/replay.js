375562fd626894aabac2a968bb9ae0fac9971935
Ryan Keathley <me@ryankeathley.com>
2024-08-25 08:21:54 -0600

    print functions

--- src/replay.js
+++ src/replay.js
@@ -14,12 +14,10 @@ function getUrlContent(url) {
         https.get(url, (response) => {
             let data = '';
 
-            // Handle data chunks
             response.on('data', (chunk) => {
                 data += chunk;
             });
 
-            // Handle end of response
             response.on('end', () => {
                 resolve(data);
             });
@@ -29,32 +27,69 @@ function getUrlContent(url) {
     });
 }
 
-function loadReplay (url) {
+function printEnginePokemon (p) {
+    console.log(p.species, p.hp, p.status);
+    for (const m of p.moves) {
+        console.log(m);
+    }
+}
+
+function printEngineBattle (battle) {
+    for (const side of battle.sides) {
+        console.log("\nSIDE:");
+        for (const p of side.pokemon) {
+            printEnginePokemon(p);
+        }
+    }
+}
+
+function printClientBattle (battle) {
+    for (const side of [battle.p1, battle.p2]) {
+        console.log('\n', side.name);
+        for (const p of side.team) {
+            console.log(p.name, p.hp, p.status, p);
+        }
+    }
+}
+
+function loadReplay(url, turn = 1000) {
     (async () => {
         try {
             const log = await getUrlContent(url);
             const lines = log.split("\n");
-            let battle = new client.Battle(new data.Generations(dex.Dex));
+            let finished_battle = new client.Battle(new data.Generations(dex.Dex));
+            let position = new client.Battle(new data.Generations(dex.Dex));
+
             for (let i = 0; i < lines.length; i = i + 1) {
                 const line = lines[i];
-                battle.add(line);
+                finished_battle.add(line);
+
+                if (position.turn < turn) {
+                    position.add(line);
+                }
             }
+
+            printClientBattle(position);
+            printClientBattle(finished_battle);
 
             const gens = new data.Generations(dex.Dex);
             const gen = gens.get(1);
-            const options =  {
-                seed : [0,0,0,0,0,0],
-                showdown : true,
-                p1 : {name : "p1", team : battle.p1.team},
-                p2 : {name : "p2", team : battle.p2.team},           
+            const options = {
+                seed: [0, 0, 0, 0, 0, 0],
+                showdown: true,
+                p1: { name: "p1", team: finished_battle.p1.team },
+                p2: { name: "p2", team: finished_battle.p2.team },
             };
 
-            engine.Battle.create(gen, options);
+            // has mons and moves but no pp or hp
+            var engine_battle = engine.Battle.create(gen, options);
+
+            printEngineBattle(engine_battle);
 
         } catch (error) {
             console.error(error);
         }
-    })();    
+    })();
 }
 
-loadReplay("https://replay.pokemonshowdown.com/smogtours-gen1ou-742822");
+loadReplay("https://replay.pokemonshowdown.com/smogtours-gen1ou-742822", 4);
