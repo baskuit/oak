9fe502441f52c5a7e512378eda33a08143dc3692
Ryan Keathley <me@ryankeathley.com>
2024-09-01 02:47:05 -0600

    add use-once formatting script for posterity

---
+++ src/format.py
@@ -0,0 +1,26 @@
+# Script to reformat a team dump I used but gitignore'd
+
+def fix_mon(mon):
+    x = mon.split('|')
+    x.insert(0, '')
+    x.insert(2, '')
+    x.insert(3, '-')
+    for _ in range(7):
+        x.append('')
+    x = '|'.join(x)
+    return x
+
+def fix_team(team):
+    mons = team.split(']')
+    mons = [fix_mon(mon) for mon in mons]
+    mons = "]".join(mons)
+    return mons
+
+in_file = open("rby.tsv", "r")
+out_file = open("rby3.tsv", "w")
+
+lines = in_file.read()
+for line in lines.strip().split('\n'):
+    r, team = line.split('\t') 
+    # out_file.write(r + '\t' + fix_team(team))
+    out_file.write(fix_team(team))
