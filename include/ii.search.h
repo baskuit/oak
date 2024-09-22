072e4c9b2d54217907c1e8234e57a8dea24362a5
Ryan Keathley <me@ryankeathley.com>
2024-09-22 16:47:58 -0600

    add wip search headers

---
+++ include/ii.search.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <atomic>
+#include <thread>
+#include <chrono>
+
+// define current Helpers::Battle based obs. Set workers to generate libpkmn battles
+
+struct Manager {
+
+    int thread_count = 32;
+
+    std::atomic<uint64_t> sampled_histories;
+
+    void start_timer() {
+        // start = std::chrono::high_resolution_clock::now();
+        sampled_histories.store(0);
+    }
+
+    float sample_rate() {
+        // const auto duration = start = std::chrono::high_resolution_clock::now() - start;
+    }
+
+    static void thread_function();
+
+};
