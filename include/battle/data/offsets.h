53ce2a7809da4617319f0f178ecbd0f641ccacc5
baskuit <baskuit@github.com>
2024-11-06 00:05:23 +0000

    add offsets

--- include/battle/data/offsets.h
+++ include/battle/data/offsets.h
@@ -1,8 +1,12 @@
 #pragma once
 
+#include <cstdint>
+
 namespace Offsets {
 
-	constexpr auto seed{376};
-	constexpr auto side{184};
+constexpr size_t seed{376};
+constexpr size_t side{184};
 
-}
+constexpr size_t pokemon{24};
+constexpr size_t order{176};
+} // namespace Offsets
