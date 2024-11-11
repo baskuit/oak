5613572787d1ee4ac808fa9d2663f451d489f753
baskuit <baskuit@github.com>
2024-11-11 12:46:15 +0000

    rm stuff, minor update

--- src/sleep.cc
+++ src/sleep.cc
@@ -1,5 +1,6 @@
 #include <battle/init.h>
 #include <data/sample-teams.h>
+#include <data/strings.h>
 
 #include <array>
 #include <iostream>
@@ -20,5 +21,18 @@ int main() {
   std::array<pkmn_choice, 9> p2_choices{};
 
   const auto battle = Init::battle(p1, p2, 123456234);
+  pkmn_gen1_options options{};
+  const auto p1_order = buffer_to_string(battle.bytes + Offsets::order, 6);
+  const auto p2_order =
+      buffer_to_string(battle.bytes + +Offsets::side + Offsets::order, 6);
+
+  pkmn_choice c1{}, c2{};
+  auto result = pkmn_battle_update(&battle, c1, c2, &options);
+  auto m =
+      pkmn_gen1_choices(&battle, PKMN_PLAYER_P1, result, p1_choices.data(), 9);
+
+  std::cout << p1_order << std::endl;
+  std::cout << p2_order << std::endl;
+
   return 0;
 }
