98f498c3f09201dfb97d548cc0c5744ba8fe1260
baskuit <baskuit@github.com>
2024-11-21 13:11:55 -0700

    eval.h progress

--- include/pi/eval.h
+++ include/pi/eval.h
@@ -1,53 +1,89 @@
 #pragma once
 
-// #include <pkmn.h>
+#include <data/moves.h>
+#include <data/species.h>
+
+#include <pi/abstract.h>
+
+#include <algorithm>
+#include <array>
 #include <assert.h>
-
-// #include ""
-
-namespace Data {
-using Species = unsigned int;
-using Moves = unsigned int;
-}; // namespace Data
-
-using pkmn_gen1_battle = uint8_t[384];
+#include <cmath>
+#include <numeric>
+#include <stddef.h>
+#include <vector>
 
 namespace Eval {
 
-// We may just use Randbat data for this
-struct FullSet {
+// The eval currently only uses hp and status information to bucket states.
+// pp, 
+constexpr size_t n_hp = 4;
+constexpr size_t n_status = 16;
+
+struct Pokemon {
   Data::Species species;
   std::array<Data::Moves, 4> moves;
-
-  void assert() const {
-    assert((moves[0] > moves[1] || moves[1] == 0) &&
-               (moves[1] > moves[2] || moves[2] == 0) &&
-               (moves[2] > moves[3] || moves[3] == 0);)
-  }
+  float hp = 1.0;
 };
 
-using SpeciesMoveSetID = uint64_t;
-static constexpr uint64_t MAX_SPECIES_MOVE_SETS{115418636672}; // 166^4 * 152
+// basic mcts util that conducts a search to populate the Mono E Mono data
+float get_value(const auto &p1, const auto &p2) { return 0; }
 
-template <typename Container>
-constexpr SpeciesMoveSetID getID(const Data::Species species,
-                                 const Container &container) {
-  auto id = species;
-  for (int i = 0; i < 4; ++i) {
-    id *= 166;
-    id += static_cast<SpeciesMoveSetID>(container[i]);
+// expected values is [0, 1]
+float from_matrix(const auto &expected_values, const auto m, const auto n) {
+  std::vector<float> p1_material;
+  std::vector<float> p2_material;
+  p1_material.resize(m);
+  p2_material.resize(n);
+
+  const auto sigmoid = [](float x) { return 1 / (1 + std::exp(-x)); };
+  const auto inv_sigmoid = [](float y) { return -std::log((1 / y) - 1); };
+
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      const auto p = expected_values[i][j];
+      const auto logit = inv_sigmoid(p);
+      constexpr auto bound = 1000;
+      // for stability
+      const auto clamped = std::max(std::min(logit, bound), -bound);
+
+      p1_material[i] += clamped / n;
+      p2_material[j] -= clamped / m;
+    }
   }
-  return id;
+
+  // we could also e.g. give extra weight to the actives
+  const float p1_sum =
+      std::accumulate(p1_material.begin(), p1_material.end(), 0);
+  const float p2_sum =
+      std::accumulate(p2_material.begin(), p2_material.end(), 0);
+  const float material_difference = (p1_sum - p2_sum) / 2;
+  return sigmoid(material_difference);
 }
 
-struct BasicMatchupEntry {
-  float value;
-  float variance;
+void from_matrix_test()
+{
+
+}
+
+using MEM = float[n_hp][n_status][n_hp][n_status];
+
+class CachedEval {
+public:
+  static constexpr auto status_index(Status status) noexcept { return 0; }
+
+  std::array<std::array<MEM, 6>, 6> mem_matrix;
+  std::array<std::array<float, 6>, 6> value_matrix;
+
+  float value() const {}
+
+  CachedEval() = default;
+
+  CachedEval(const pkmn_gen1_battle &battle) {}
+
+  CachedEval(const pkmn_gen1_battle &battle, const auto &global) {}
 };
 
-// Holds values/variance for say any two species
-struct SpeciesMatchupTable {
-  BasicMatchupEntry data[3][3];
-};
+struct GlobalMEM {};
 
 }; // namespace Eval
