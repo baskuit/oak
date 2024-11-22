9305d23316d3c52bf90d24437e65b317e7f0778e
baskuit <baskuit@github.com>
2024-11-22 12:04:06 -0700

    WIP eval (hp only)

--- include/pi/eval.h
+++ include/pi/eval.h
@@ -3,7 +3,12 @@
 #include <data/moves.h>
 #include <data/species.h>
 
+#include <battle/init.h>
 #include <pi/abstract.h>
+#include <pi/exp3.h>
+#include <pi/mcts.h>
+#include <pi/tree.h>
+#include <util/random.h>
 
 #include <algorithm>
 #include <array>
@@ -14,6 +19,7 @@
 #include <fstream>
 #include <map>
 #include <numeric>
+#include <type_traits>
 #include <vector>
 
 namespace Eval {
@@ -30,59 +36,36 @@ struct Pokemon {
 };
 
 // basic mcts util that conducts a search to populate the Mono E Mono data
-float get_value(const auto &p1, const auto &p2) { return 0; }
-
-// expected values is [0, 1]
-float from_matrix(const auto &expected_values, const auto m, const auto n) {
-  std::vector<float> p1_material;
-  std::vector<float> p2_material;
-  p1_material.resize(m);
-  p2_material.resize(n);
-
-  const auto sigmoid = [](float x) { return 1 / (1 + std::exp(-x)); };
-  const auto inv_sigmoid = [](float y) { return -std::log((1 / y) - 1); };
-
-  for (int i = 0; i < m; ++i) {
-    for (int j = 0; j < n; ++j) {
-      const auto p = expected_values[i][j];
-      const auto logit = inv_sigmoid(p);
-      constexpr auto bound = 1000;
-      // for stability
-      const auto clamped = std::max(std::min(logit, bound), -bound);
-
-      p1_material[i] += clamped / n;
-      p2_material[j] -= clamped / m;
-    }
-  }
-
-  // we could also e.g. give extra weight to the actives
-  const float p1_sum =
-      std::accumulate(p1_material.begin(), p1_material.end(), 0);
-  const float p2_sum =
-      std::accumulate(p2_material.begin(), p2_material.end(), 0);
-  const float material_difference = (p1_sum - p2_sum) / 2;
-  return sigmoid(material_difference);
+float get_value(const auto &set1, const auto &set2, size_t iterations,
+                auto seed) {
+  std::array<std::remove_reference_t<decltype(set1)>, 1> p1{set1};
+  std::array<std::remove_reference_t<decltype(set2)>, 1> p2{set2};
+  prng device{seed};
+  auto battle = Init::battle(p1, p2, device.uniform_64());
+  using Node =
+      Tree::Node<Exp3::JointBanditData<false>, std::array<uint8_t, 16>>;
+  Node node{};
+  MCTS<false, true> search;
+  auto result = Init::update(battle, 0, 0, search.options);
+  pkmn_gen1_chance_durations durations{};
+  const auto output =
+      search.run(iterations, device, node, &battle, result, &durations);
+  return output.average_value;
 }
-
-void from_matrix_test() {}
 
 using MEM = std::array<std::array<float, n_hp>, n_hp>;
 
-class CachedEval {
-public:
-  static constexpr auto status_index(Status status) noexcept { return 0; }
-
-  std::array<std::array<MEM, 6>, 6> mem_matrix;
-  std::array<std::array<float, 6>, 6> value_matrix;
-
-  float value() const {}
-
-  CachedEval() = default;
-
-  CachedEval(const pkmn_gen1_battle &battle) {}
-
-  CachedEval(const pkmn_gen1_battle &battle, const auto &global) {}
-};
+MEM compute_table(auto set1, auto set2, const auto seed) {
+  MEM result;
+  for (int hp1 = 0; hp1 < 3; ++hp1) {
+    for (int hp2 = 0; hp2 < 3; ++hp2) {
+      set1.hp = hp1 / 4.0;
+      set2.hp = hp2 / 4.0;
+      result[hp1][hp2] = get_value(set1, set2, 1 << 20, seed);
+    }
+  }
+  return result;
+}
 
 class GlobalMEM {
 public:
@@ -119,16 +102,17 @@ public:
     return id;
   }
 
-  const MEM &operator()(const auto &p1_set, const auto &p2_set) const {
-    SetID id1 = toID(p1_set);
-    SetID id2 = toID(p2_set);
-    // if (id1 > id2) {
+  // const MEM &operator()(const auto &p1_set, const auto &p2_set) const {
+  //   SetID id1 = toID(p1_set);
+  //   SetID id2 = toID(p2_set);
+  //   // if (id1 > id2) {
 
-    // } else {
+  //   // } else {
 
-    // }
-    // return MEMData.at({});
-  }
+  //   // }
+  //   // return MEMData.at({});
+
+  // }
 
   bool save(const std::filesystem::path path) const {
     std::ofstream file(path, std::ios::binary | std::ios::trunc);
@@ -172,5 +156,55 @@ public:
 private:
   std::map<std::pair<SetID, SetID>, MEM> MEMData;
 };
+
+class CachedEval {
+public:
+  static constexpr auto status_index(Abstract::Status status) noexcept {
+    return 0;
+  }
+
+  std::array<std::array<MEM, 6>, 6> mem_matrix;
+  std::array<std::array<float, 6>, 6> value_matrix;
+
+  float value() const { return 0; }
+
+  CachedEval() = default;
+
+  CachedEval(const pkmn_gen1_battle &battle) {}
+
+  CachedEval(const pkmn_gen1_battle &battle, const auto &global) {}
+};
+
+// expected values is [0, 1]
+float from_matrix(const auto &expected_values, const auto m, const auto n) {
+  std::vector<float> p1_material;
+  std::vector<float> p2_material;
+  p1_material.resize(m);
+  p2_material.resize(n);
+
+  const auto sigmoid = [](float x) { return 1 / (1 + std::exp(-x)); };
+  const auto inv_sigmoid = [](float y) { return -std::log((1 / y) - 1); };
+
+  for (int i = 0; i < m; ++i) {
+    for (int j = 0; j < n; ++j) {
+      const auto p = expected_values[i][j];
+      const auto logit = inv_sigmoid(p);
+      constexpr auto bound = 1000;
+      // for stability
+      const auto clamped = std::max(std::min(logit, bound), -bound);
+
+      p1_material[i] += clamped / n;
+      p2_material[j] -= clamped / m;
+    }
+  }
+
+  // we could also e.g. give extra weight to the actives
+  const float p1_sum =
+      std::accumulate(p1_material.begin(), p1_material.end(), 0);
+  const float p2_sum =
+      std::accumulate(p2_material.begin(), p2_material.end(), 0);
+  const float material_difference = (p1_sum - p2_sum) / 2;
+  return sigmoid(material_difference);
+}
 
 }; // namespace Eval
--- src/eval.cc
+++ src/eval.cc
@@ -3,38 +3,58 @@
 #include <data/options.h>
 #include <data/sample-teams.h>
 #include <data/strings.h>
-#include <pi/abstract.h>
-#include <util/random.h>
-
 #include <iostream>
+#include <pi/abstract.h>
+#include <pi/eval.h>
+#include <util/random.h>
 
 static_assert(Options::calc && Options::chance && !Options::log);
 
 int abstract_test(int argc, char **argv) {
   if (argc != 3) {
-    std::cout << "Usage: provide two team indices to start Abstract test"
-              << std::endl;
+    std::cout
+        << "Usage: provide seed and tries for Abstract::Battle update test"
+        << std::endl;
     return 1;
   }
-  const uint64_t seed = 192380129381;
-  const int i = std::atoi(argv[1]);
-  const int j = std::atoi(argv[2]);
-
+  const uint64_t seed = std::atoi(argv[1]);
+  const size_t tries = std::atoi(argv[2]);
   prng device{seed};
+  for (size_t t = 0; t < tries; ++t) {
 
-  auto battle = Init::battle(SampleTeams::teams[i], SampleTeams::teams[j]);
-  pkmn_gen1_battle_options options;
-  auto result = Init::update(battle, 0, 0, options);
-  Abstract::Battle abstract{battle};
+    const int i = device.random_int(SampleTeams::teams.size());
+    const int j = device.random_int(SampleTeams::teams.size());
 
-  int t = 0;
-  while (!pkmn_result_type(result)) {
-    const auto [choices1, choices2] = Init::choices(battle, result);
-    auto c1 = choices1[device.random_int(choices1.size())];
-    auto c2 = choices2[device.random_int(choices2.size())];
-    result = Init::update(battle, c1, c2, options);
-    ++t;
+    auto battle = Init::battle(SampleTeams::teams[i], SampleTeams::teams[j]);
+    pkmn_gen1_battle_options options;
+    auto result = Init::update(battle, 0, 0, options);
+    Abstract::Battle abstract{battle};
+
+    int turns = 0;
+    while (!pkmn_result_type(result)) {
+      const auto [choices1, choices2] = Init::choices(battle, result);
+      auto c1 = choices1[device.random_int(choices1.size())];
+      auto c2 = choices2[device.random_int(choices2.size())];
+      result = Init::update(battle, c1, c2, options);
+
+      abstract.update(battle);
+      Abstract::Battle new_abstract{battle};
+      assert(abstract == new_abstract);
+      ++turns;
+    }
+    // std::cout << turns << std::endl;
   }
+
+  Eval::Pokemon lax{
+      Species::Snorlax,
+      {Moves::BodySlam, Moves::Rest, Moves::Earthquake, Moves::Reflect},
+      .1};
+  Eval::Pokemon chansey{
+      Species::Chansey,
+      {Moves::SeismicToss, Moves::Rest, Moves::Earthquake, Moves::Reflect},
+      1};
+
+  std::cout << Eval::get_value(lax, chansey, 1 << 20, 123123) << std::endl;
 
   return 0;
 }
