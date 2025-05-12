b176d20b942408c4f4d2b8c59745e01d862ffb48
--global <--global>
2025-05-12 13:08:54 -0600

    trash commit

--- include/nnue/main.py
+++ include/nnue/main.py
@@ -1,57 +1,52 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
+# import torch
+# import torch.nn as nn
+# import torch.optim as optim
 
-from enum import Enum
+from random import randint, sample, choices
+from math import ceil
 
-# Pokemon Constants
-GHASTLY_HP = 324
-HAUNTER_HP = 294
-GENGAR_HP = 264
+from moves import Move, legal_moves
 
-HP_ARRAY = [GHASTLY_HP, HAUNTER_HP, GENGAR_HP]
-
-class Species(Enum):
-    GHASTLY = 0
-    HAUNTER = 1
-    GENGAR = 2
-
-class Request(Enum):
-    PASS = 0
-    MOVE = 1
-    SWITCH = 2
-
-class Pokemon(Enum):
-    def __init__(self):
-        self.species : Species
-        self.hp : int
-
-    def to_tensor(self) -> torch.tensor:
-        return torch.zeros((1, 256))
+class Pokemon:
+    def __init__(self, fainted = False):
+        self.moves = {Move.NightShade}
+        for _ in range(3):
+            m = choices(legal_moves, k=1)
+            self.moves.add(m[0])
+        if not fainted:
+            self.hp = randint(1, 353)
+        else:
+            self.hp = 0
+    
+    def hits(self):
+        return ceil(self.hp / 100)
 
 class Side:
-    def __init__(self):
-        self.active : Pokemon
-        self.bench : Pokemon[5]
+    def __init__(self, switch = False, b = 5):
+        self.active = Pokemon(switch)
+        self.bench = []
+        for _ in range(b):
+            self.bench.append(Pokemon())
 
-    def to_tensor(self):
-        t = self.active.to_tensor()
-        for p in self.bench:
-            t += p.to_tensor()
-        return t
-    
-    def hits(self) -> 
+    def hits(self):
+        s = self.active.hits()
+        for mon in self.bench:
+            s += mon.hits()
+        return s
 
 class Battle:
-    def __init__(self):
-        self.p1 : Side
-        self.p2 : Side
+    def __init__(self, request=0):
+        self.p1 = Side(request == 1)
+        self.p2 = Side(request == 2)
 
-    def to_tensor(self) -> torch.tensor:
-        return torch.concat([self.p1.to_tensor(), self.p2.to_tensor()], dim=1)
-
-def generate_battle() -> Battle:
-    return Battle()
+    def score(self):
+        diff = self.p1.hits() - self.p2.hits()
+        if diff < 0:
+            return 0.0
+        elif diff == 0:
+            return 0.5
+        else:
+            return 1.0
 
 # Net Constants
 ACTIVE_INPUT_SIZE = 3 + 3 + 151 + 161  # result, hp, species, moves
@@ -59,27 +54,9 @@ BENCH_INPUT_SIZE = 3 + 151 + 161  # hp, species, moves
 HIDDEN_SIZE = 512
 OUTPUT_SIZE = 256
 
-class TwoLayerMLP(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super().__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = self.relu(x)
-        x = torch.clamp(x, max=1.0)
-        x = self.fc2(x)
-        return x
-
-class ActiveNet(TwoLayerMLP):
-    def __init__(self):
-        super().__init__(ACTIVE_INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
-
-class BenchNet(TwoLayerMLP):
-    def __init__(self):
-        super().__init__(BENCH_INPUT_SIZE, HIDDEN_SIZE, OUTPUT_SIZE)
 
 if __name__ == "__main__":
-    pass
+    b = Battle()
+    print(b.p1.bench)
+    print(b.p2.bench)
+    print(b.score())
--- src/eval.cc
+++ src/eval.cc
@@ -1,3 +1,4 @@
+#include <data/durations.h>
 #include <data/options.h>
 
 #include <battle/sample-teams.h>
@@ -15,54 +16,104 @@
 static_assert(Options::calc && Options::chance && !Options::log);
 
 template <typename Team, typename Dur>
-void versus(std::atomic<int> *index, Eval::OVODict *eval_dict,  size_t max, Dur dur, uint64_t seed,
+void versus(std::atomic<int> *index, size_t max, Dur dur, uint64_t seed,
             size_t *n, size_t *score) {
 
   while (index->fetch_add(1) < max) {
 
-    const auto half = [dur, eval_dict](auto side1, auto side2, auto seed) -> float {
-      auto battle = Init::battle(side1, side2);
-      pkmn_gen1_battle_options options{};
+    const auto half = [dur](auto x, auto y, auto seed) -> float {
+      auto battle = Init::battle(x, y);
       pkmn_gen1_chance_durations durations{};
+      pkmn_gen1_battle_options options{};
       auto result = Init::update(battle, 0, 0, options);
 
+      Eval::OVODict global{};
+      global.load("./cache");
+
       MCTS search{};
-      Eval::Model eval{seed, Eval::CachedEval{side1, side2, *eval_dict}};
+      MonteCarlo::Model mcm{seed};
+      Eval::Model eval{mcm.device.uniform_64(), Eval::CachedEval{x, y, global}};
+      
+      std::vector<float> mcts_value_for_eval_side{};
+      std::vector<float> eval_values{};
 
       while (!pkmn_result_type(result)) {
 
         const auto [choices1, choices2] = Init::choices(battle, result);
 
         auto i = 0;
-        auto j = 0;
-        if ((choices1.size() > 1) || (choices2.size() > 1)) {
+        if (choices1.size() > 1) {
           using Node = Tree::Node<Exp3::JointBanditData<.03f, false>,
                                   std::array<uint8_t, 16>>;
           Node node{};
-          Eval::Input input{battle, durations, 
-            Eval::Abstract{battle, eval.eval.ovo_matrix}, result};
-          auto output =
-              search.run<MCTS::Options<true, 3, 3, 1>>(dur, node, input, eval);
-          i = eval.device.sample_pdf(output.p1);
-          j = eval.device.sample_pdf(output.p2);
+          MonteCarlo::Input input1{battle, durations, result};
+          auto output1 =
+              search.run<MCTS::Options<true, 3, 3, 1>>(dur, node, input1, mcm);
+          i = mcm.device.sample_pdf(output1.p1);
+          mcts_value_for_eval_side.push_back(1 - output1.average_value);
+        } else {
+          mcts_value_for_eval_side.push_back(-1);
         }
+
+        auto j = 0;
+        if (choices2.size() > 1) {
+          using Node = Tree::Node<Exp3::JointBanditData<.03f, false>,
+                                  std::array<uint8_t, 16>>;
+          Node node{};
+          Eval::Input input2{battle, durations,
+                             Eval::Abstract{battle, eval.eval.ovo_matrix},
+                             result};
+          // MonteCarlo::Input input2{battle, durations, result};
+          auto output2 =
+              search.run<MCTS::Options<true, 3, 3, 1>>(dur, node, input2, eval);
+          j = eval.device.sample_pdf(output2.p2);
+          // input2.abstract.print();
+          const auto v = eval.eval.value(input2.abstract);
+          // std::cout << v << std::endl;
+          // std::cout << "iter: " << output2.iterations << std::endl;
+          eval_values.push_back(1 - v);
+        } else {
+          // std::cout << "-" << std::endl;
+          eval_values.push_back(-1);
+        }
+
+        // std::cout << Strings::battle_to_string(battle) << std::endl;
+
         result = Init::update(battle, choices1[i], choices2[j], options);
         durations = *pkmn_gen1_battle_options_chance_durations(&options);
       }
+      const auto v = eval_values.size();
+      for (auto vi = 0; vi < v; ++vi) {
+        std::cout << mcts_value_for_eval_side[vi] << " ~ " << eval_values[vi] << '\t';
+      }
       const auto score = Init::score(result);
+      std::cout << "~ " << 1 - score << std::endl;
+
       return score;
     };
 
     prng device{seed};
-    const auto n_teams = SampleTeams::teams.size();
-    const auto p1 = SampleTeams::teams[device.random_int(n_teams)];
-    const auto p2 = SampleTeams::teams[device.random_int(n_teams)];
+
+    const auto p1 = SampleTeams::teams[device.random_int(100)];
+    const auto p2 = SampleTeams::teams[device.random_int(100)];
+
     *score += 2 * half(p1, p2, device.uniform_64());
+    *n += 1;
+    *score += 2 * half(p2, p1, device.uniform_64());
     *n += 1;
   }
 }
 
-int generate_ovo_games(int argc, char **argv) {
+void print_score(bool *flag, size_t *n, size_t *score) {
+  while (!*flag) {
+    float n_ = static_cast<float>(*n) + 1.0f * (*n == 0);
+    float avg = static_cast<float>(*score) / 2 / n_;
+    std::cout << "n: " << *n << " avg: " << avg << std::endl;
+    sleep(10);
+  }
+}
+
+int abstract_test(int argc, char **argv) {
 
   using Team = std::array<Init::Set, 6>;
 
@@ -84,28 +135,20 @@ int generate_ovo_games(int argc, char **argv) {
   max = std::atoi(argv[3]);
   seed = std::atoi(argv[4]);
 
-  std::cout << "Input - " << "ms: " <<  ms << " threads: " << threads << " max: " << max << " seed: " << seed
+  std::cout << "Input: " << ms << ' ' << threads << ' ' << max << ' ' << seed
             << std::endl;
 
   std::atomic<int> index{};
   prng device{seed};
-
 
   size_t n = 0;
   size_t score = 0;
 
   std::thread thread_pool[threads];
 
-  Eval::OVODict eval_dict{};
-  if(!eval_dict.load("./cache")){
-    std::cerr << "could not load cache" << std::endl;
-    return 1;
-  }
-
   for (auto t = 0; t < threads; ++t) {
     thread_pool[t] = std::thread{&versus<Team, std::chrono::milliseconds>,
                                  &index,
-                                 &eval_dict,
                                  max,
                                  std::chrono::milliseconds{ms},
                                  device.uniform_64(),
@@ -113,14 +156,18 @@ int generate_ovo_games(int argc, char **argv) {
                                  &score};
   }
 
-  // TODO period save to disc thread
-  // TODO ctrl+c cleanup
+  bool flag = false;
+
+  std::thread print_thread{&print_score, &flag, &n, &score};
 
   for (auto t = 0; t < threads; ++t) {
     thread_pool[t].join();
   }
 
+  flag = true;
+  print_thread.join();
+
   return 0;
 }
 
-int main(int argc, char **argv) { return generate_ovo_games(argc, argv); }
+int main(int argc, char **argv) { return abstract_test(argc, argv); }
