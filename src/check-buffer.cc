1d7f1f04b2de1f024a099dcc2e702772a327ae24
--global <--global>
2025-05-29 13:59:28 -0600

    cf

--- src/check-buffer.cc
+++ src/check-buffer.cc
@@ -1,7 +1,7 @@
-#include <data/durations.h>
 #include <battle/sample-teams.h>
 #include <battle/strings.h>
 #include <battle/view.h>
+#include <data/durations.h>
 
 #include <util/print.h>
 
@@ -35,37 +35,39 @@ static_assert(sizeof(Frame) == 405);
 
 int main(int argc, char **argv) {
 
-    if (argc != 3) {
-        std::cerr << "Input: filename, number of frames" << std::endl;
-        return -1;
-    }
+  if (argc != 3) {
+    std::cerr << "Input: filename, number of frames" << std::endl;
+    return -1;
+  }
 
-    std::string fn{argv[1]};
-    std::cout << fn << std::endl;
+  std::string fn{argv[1]};
+  std::cout << fn << std::endl;
 
-    int fd = open(fn.data(), O_RDONLY);
-    std::cout << "fd: " << fd << std::endl;
-    if (fd == -1) {
-      std::cerr << "Failed to open file" << std::endl;
-      return -1;
-    }
-    size_t length = std::atoi(argv[2]);
-    std::cout << "length: " << length <<std::endl;
+  int fd = open(fn.data(), O_RDONLY);
+  std::cout << "fd: " << fd << std::endl;
+  if (fd == -1) {
+    std::cerr << "Failed to open file" << std::endl;
+    return -1;
+  }
+  size_t length = std::atoi(argv[2]);
+  std::cout << "length: " << length << std::endl;
 
-    uint8_t* bytes = new uint8_t[length * sizeof(Frame)];
-    const auto r = pread(fd, bytes, length * sizeof(Frame), 0);
-    if (r == -1) {
-      std::cerr << "pread Error." << std::endl;
-      return -1;
-    }
+  uint8_t *bytes = new uint8_t[length * sizeof(Frame)];
+  const auto r = pread(fd, bytes, length * sizeof(Frame), 0);
+  if (r == -1) {
+    std::cerr << "pread Error." << std::endl;
+    return -1;
+  }
 
-    for (auto i = 0; i < length; ++i) {
-      const auto& frame= *reinterpret_cast<const Frame*>(bytes + (sizeof(Frame) * i));
-      std::cout << i << ":\n";
-      pkmn_gen1_battle battle;
-      std::memcpy(battle.bytes, frame.battle.data(), Sizes::battle);
-      std::cout << Strings::battle_to_string(battle) << '\n';
-      std::cout << "eval: " << frame.eval << "; iter: " << frame.iter << "; score: " << frame.score << '\n';
-    }
-    return 0;
+  for (auto i = 0; i < length; ++i) {
+    const auto &frame =
+        *reinterpret_cast<const Frame *>(bytes + (sizeof(Frame) * i));
+    std::cout << i << ":\n";
+    pkmn_gen1_battle battle;
+    std::memcpy(battle.bytes, frame.battle.data(), Sizes::battle);
+    std::cout << Strings::battle_to_string(battle) << '\n';
+    std::cout << "eval: " << frame.eval << "; iter: " << frame.iter
+              << "; score: " << frame.score << '\n';
+  }
+  return 0;
 }
