187a31d71b2e13ae8310caec093ddbcdb9c0b377
--global <--global>
2025-06-04 10:22:56 -0600

    jesus

--- src/train.py
+++ src/train.py
@@ -6,6 +6,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+        
+def raw_save(tensor, path):
+    with open(path, "wb") as f:
+        f.write(tensor.numpy().tobytes())
 
 class ClampedReLU(nn.Module):
     def forward(self, x):
@@ -14,11 +18,13 @@ class ClampedReLU(nn.Module):
 class TwoLayerMLP(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(TwoLayerMLP, self).__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
         self.activation = ClampedReLU()
+        self.fc0 = nn.Linear(input_dim, hidden_dim)
+        self.fc1 = nn.Linear(hidden_dim, hidden_dim)
         self.fc2 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
+        x = self.activation(self.fc0(x))
         x = self.activation(self.fc1(x))
         x = self.fc2(x)
         return x
@@ -30,35 +36,45 @@ class TwoLayerMLP(nn.Module):
         state_dict = torch.load(path, map_location=map_location)
         self.load_state_dict(state_dict)
 
-def raw_write(tensor, path):
-    with open(path, "wb") as f:
-        f.write(tensor.numpy().tobytes())
+    def clamp_weights(self):
+        with torch.no_grad():
+            self.fc0.weight.clamp_(-2, 2)
+            self.fc1.weight.clamp_(-2, 2)
+            self.fc2.weight.clamp_(-2, 2)
+
+    def save_quantized(self, path):
+        raw_save((self.fc0.weight * (127)).to(torch.int8), os.path.join(path, "w0"))
+        raw_save((self.fc1.weight * (127)).to(torch.int8), os.path.join(path, "w1"))
+        raw_save((self.fc2.weight * (127)).to(torch.int8), os.path.join(path, "w2"))
+        raw_save((self.fc0.bias * (127 * 64)).to(torch.int32), os.path.join(path, "b0"))
+        raw_save((self.fc1.bias * (127 * 64)).to(torch.int32), os.path.join(path, "b1"))
+        raw_save((self.fc2.bias * (127 * 64)).to(torch.int32), os.path.join(path, "b2"))
+
 
 def init_weights():
     acc = torch.rand(512).clamp_(0, 1)
     torch.save(acc, "./weights/acc.pt")
     accd = (acc * 127).to(torch.int8)
-    raw_write(accd, "./weights/accd")    
+    raw_save(accd, "./weights/accd")    
 
     net = TwoLayerMLP(512, 32, 1)
-    with torch.no_grad():
-        for param in net.parameters():
-            param.clamp_(-2, 2)
+    net.clamp_weights   ()
     net.save("./weights/nn.pt")
-
-    w1d = (net.fc1.weight * (127)).to(torch.int8)
-    b1d = (net.fc1.bias * (127 * 64)).to(torch.int32)
-    w2d = (net.fc2.weight * (127)).to(torch.int8)
-    b2d = (net.fc2.bias * (127 * 64)).to(torch.int32)
-
-    raw_write(w1d, "./weights/w1d")
-    raw_write(b1d, "./weights/b1d")
-    raw_write(w2d, "./weights/w2d")
-    raw_write(b2d, "./weights/b2d")
+    net.save_quantized("./weights")
 
     print(acc[:10])
     print(accd[:10])
-    
+
+def test():
+    net = TwoLayerMLP(512, 32, 1)
+    net.load("./weights/nn.pt")
+    # acc = torch.load("./weights/acc.pt")
+    # x = (net.fc1.forward(acc) * 127).to(torch.uint8)
+    # print(x)
+    # print(net.fc1.bias)
+    print(net.fc2.weight.detach().numpy())
+    print((net.fc2.weight * 64).to(torch.int8))
+
 
 def train():
     # Dummy data
