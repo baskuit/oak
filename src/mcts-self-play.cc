fbd9d45284d38335c79ae60a4a60ab2a81e2d64d
--global <--global>
2025-06-05 14:01:56 -0600

    add max_teams option, Options namespace- change defaults finish arg parsing. print team data

--- src/mcts-self-play.cc
+++ src/mcts-self-play.cc
@@ -28,20 +28,21 @@ static_assert(Options::calc && Options::chance && !Options::log);
 
 bool terminated = false;
 bool suspended = false;
-constexpr size_t max_teams = SampleTeams::teams.size();
+constexpr size_t n_teams = SampleTeams::teams.size();
 
 namespace Stats {
-std::array<std::atomic<size_t>, max_teams> sample_counts{};
-std::array<std::array<std::atomic<size_t>, max_teams>, max_teams>
-    matchup_counts{};
-std::array<std::array<std::atomic<size_t>, max_teams>, max_teams>
-    matchup_scores{};
+std::array<std::atomic<size_t>, n_teams> sample_counts{};
+std::array<std::array<std::atomic<size_t>, n_teams>, n_teams> matchup_counts{};
+std::array<std::array<std::atomic<size_t>, n_teams>, n_teams> matchup_scores{};
 } // namespace Stats
 
+namespace Options {
 bool keep_node = false;
 bool prune = false;
 std::string randomize_teams = "off";
-double sample_temp = 2.0;
+double sample_temp = 1.0;
+size_t max_teams = n_teams;
+}; // namespace Options
 
 struct GameBuffer {
 
@@ -116,12 +117,12 @@ void generate(int fd, std::atomic<size_t> *write_index,
   const auto random_index = [&device](size_t n, double sample_temp) {
     double r = pow(device.uniform() * pow(n, sample_temp), 1 / sample_temp);
     auto m = static_cast<size_t>(r);
-    return std::max<size_t>(0, n - (m + 1));
+    return std::max(size_t{0}, n - (m + 1));
   };
 
   const auto get_new_node = [](auto &unique_node, auto i1, auto i2,
                                const auto &obs) {
-    if (keep_node) {
+    if (Options::keep_node) {
       auto *child = (*unique_node)[i1, i2, obs];
       if (!child) {
         unique_node = std::make_unique<Node>();
@@ -138,7 +139,7 @@ void generate(int fd, std::atomic<size_t> *write_index,
   const auto think_time = [&]() { return dur; };
 
   const auto prune_low_probs = [](auto &p) {
-    if (!prune) {
+    if (!Options::prune) {
       return;
     }
     const double t = 1.0 / p.size();
@@ -159,8 +160,8 @@ void generate(int fd, std::atomic<size_t> *write_index,
   };
 
   while (true) {
-    auto t1 = random_index(max_teams, sample_temp);
-    auto t2 = random_index(max_teams, sample_temp);
+    auto t1 = random_index(Options::max_teams, Options::sample_temp);
+    auto t2 = random_index(Options::max_teams, Options::sample_temp);
     if (t1 > t2) {
       std::swap(t1, t2);
     }
@@ -265,7 +266,7 @@ void handle_print(std::atomic<size_t> *frame_count) {
     const auto more = frame_count->load();
     std::cout << (more - done) / (float)sec << " samples/sec." << std::endl;
     done = more;
-    // for (auto i = 0; i < max_teams; ++i) {
+    // for (auto i = 0; i < n_teams; ++i) {
     //   std::cout << Stats::sample_counts[i] << ' ';
     // }
     // std::cout << std::endl;
@@ -304,29 +305,49 @@ int allocate_buffer(std::string filename, size_t global_buffer_size) {
 
 int main(int argc, char **argv) {
 
+  const auto set_bool = [](bool &b, std::string s) {
+    if (s == "true" || s == "t") {
+      b = true;
+    } else if (s == "false" || s == "f") {
+      b = false;
+    } else {
+      return 1;
+    }
+    return 0;
+  };
+
   std::vector<std::string> pArgs;
   for (int i = 1; i < argc; ++i) {
     std::string arg = argv[i];
     if (arg.starts_with("--")) {
       if (arg.starts_with("--keep-node=")) {
         std::string val = arg.substr(12);
-        if (val == "true") {
-          keep_node = true;
-        } else if (val == "false") {
-          keep_node = false;
-        } else {
-          std::cerr << "Invalid value for --keep-node: " << val << "\n";
+        if (set_bool(Options::keep_node, val)) {
+          std::cerr << "Invalid value for --keep-node: " << val << '\n';
           return 1;
         }
+      } else if (arg.starts_with("--prune=")) {
+        std::string val = arg.substr(8);
+        if (set_bool(Options::prune, val)) {
+          std::cerr << "Invalid value for --prune: " << val << '\n';
+          return 1;
+        }
+      } else if (arg.starts_with("--sample-temp=")) {
+        std::string val = arg.substr(14);
+        Options::sample_temp = std::stof(val);
       } else if (arg.starts_with("--randomize=")) {
-        randomize_teams = arg.substr(12);
-        if (randomize_teams != "off" && randomize_teams != "sets") {
-          std::cerr << "Invalid value for --randomize: " << randomize_teams
-                    << "\n";
+        Options::randomize_teams = arg.substr(12);
+        if (Options::randomize_teams != "off" &&
+            Options::randomize_teams != "sets") {
+          std::cerr << "Invalid value for --randomize: "
+                    << Options::randomize_teams << '\n';
           return 1;
         }
+      } else if (arg.starts_with("--max-teams=")) {
+        const size_t n = std::stoi(arg.substr(12));
+        Options::max_teams = std::min(n_teams, std::max(size_t{1}, n));
       } else {
-        std::cerr << "Unknown flag: " << arg << "\n";
+        std::cerr << "Unknown flag: " << arg << '\n';
         return 1;
       }
     } else {
@@ -343,12 +364,16 @@ int main(int argc, char **argv) {
   size_t global_buffer_size = std::atoi(pArgs[2].c_str());
   uint64_t seed = std::atoi(pArgs[3].c_str());
 
-  std::cout << "threads: " << threads << "\n";
-  std::cout << "ms: " << ms << "\n";
-  std::cout << "max_frames: " << global_buffer_size << "\n";
-  std::cout << "seed: " << seed << "\n";
-  std::cout << "keep_node: " << (keep_node ? "true" : "false") << "\n";
-  std::cout << "randomize: " << randomize_teams << "\n";
+  std::cout << "threads: " << threads << '\n';
+  std::cout << "ms: " << ms << '\n';
+  std::cout << "max_frames: " << global_buffer_size << '\n';
+  std::cout << "seed: " << seed << '\n';
+  std::cout << "Options:\n";
+  std::cout << "keep-node: " << (Options::keep_node ? "true" : "false") << '\n';
+  std::cout << "randomize: " << Options::randomize_teams << '\n';
+  std::cout << "prune: " << Options::prune << '\n';
+  std::cout << "sample-temp: " << Options::sample_temp << '\n';
+  std::cout << "max-teams: " << Options::max_teams << '\n';
 
   std::signal(SIGINT, handle_terminate);
   std::signal(SIGTSTP, handle_suspend);
@@ -382,6 +407,27 @@ int main(int argc, char **argv) {
             << std::endl;
   if (ftruncate(buffer_fd, frames_generated * sizeof(Frame)) != 0) {
     std::cerr << "Failed to truncate final buffer." << std::endl;
+  }
+
+  std::array<std::pair<size_t, size_t>, n_teams> team_score_data{};
+  std::array<double, n_teams> team_score{};
+  for (auto i = 0; i < Options::max_teams; ++i) {
+    for (auto j = i + 1; j < Options::max_teams; ++j) {
+      const auto n = Stats::matchup_counts[i][j].load();
+      const auto v = Stats::matchup_scores[i][j].load();
+      team_score_data[i].first += n;
+      team_score_data[j].first += n;
+      team_score_data[i].second += v;
+      team_score_data[j].second += (2 * n) - v;
+    }
+  }
+  std::cout << "Team data:\n";
+  for (auto i = 0; i < Options::max_teams; ++i) {
+    team_score[i] =
+        team_score_data[i].second / (double)team_score_data[i].first;
+    team_score[i] /= 2;
+    std::cout << i << "- v: " << team_score[i]
+              << " n: " << Stats::sample_counts[i].load() << '\n';
   }
 
   return 0;
